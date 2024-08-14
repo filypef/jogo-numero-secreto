01c727c134f426e3b76a9df1ea45aaaed6f426de
Filype F. <filypef@gmail.com>
2024-08-13 21:27:18 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,82 @@
+/*let numberSecret = 2;
+
+let numberWrite = prompt('Write a number?');
+
+if (numberWrite == numberSecret){
+    alert(`Você acertou! O número secreto é: ${numberSecret}`)
+}else{
+    alert(`Você errou o número secreto era: ${numberSecret}`)
+}*/
+
+
+/*alert('Boas vindas ao nosso site');
+
+let nome = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+
+alert('Erro! Preencha todos os campos');
+
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+
+alert(mensagemDeErro);
+
+nome = prompt('Informe o seu nome');
+idade = prompt('Informe sua idade');
+
+if(idade >= 18){
+    alert('Pode tirar a habilitacao');
+}else{
+    alert('Nao pode tirar habilitacao');
+}*/
+
+
+/*let dayWeek = prompt('Qual o dia da semana?');
+
+if(dayWeek == 'Sábado' || dayWeek == 'Domingo'){
+    alert('Bom final de semana');
+}else{
+    alert('Boa semana');
+}*/
+
+/*let numeroDigitado = prompt1]1('Digite um número');
+
+if(numeroDigitado >= 0){
+    alert('Positivo');
+}else{
+    alert('Negativo');
+}*/
+
+/*let saldoEmConta = 1000;
+
+alert(`O seu dado em conta é de R$ ${saldoEmConta}`);*/
+
+
+/*let contador = 1;
+
+while(contador <= 10){
+    console.log(`O valor do contador é: ${contador}`);
+    contador++;
+}*/
+
+/*let contador = 10
+
+while(contador >= 1){
+    console.log(`O valor do contador é: ${contador}`);
+    contador--;
+}*/
+
+/*let contador = prompt('Digite um número');
+
+while(contador>0){
+    console.log(`Faltam ${contador} dias`);
+    contador--;
+}*/
+
+let numero = prompt('Digite um número');
+let contador = 0;
+
+while(contador <= numero){
+    console.log(`Faltam ${contador} dias`);
+    contador++;
+}
